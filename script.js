c28b037b43bf1aca35f39694c7a0b1bbbd49a2b2
vl0526 <156920571+vl0526@users.noreply.github.com>
2024-08-02 16:07:55 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function openDouyin() {
+    window.open('https://www.douyin.com/search/沙雕动画', '_blank');
+    window.open('https://bravedown.com/douyin-video-downloader', '_blank');
+}
+
+function openIxigua() {
+    window.open('https://www.ixigua.com/search/沙雕动画', '_blank');
+    window.open('https://bravedown.com/ixigua-video-downloader', '_blank');
+}
+
+function openBilibili() {
+    window.open('https://bravedown.com/bilibili-downloader', '_blank');
+    window.open('https://search.bilibili.com/all?keyword=沙雕动画', '_blank');
+}
+
+function openCapCut() {
+    window.open('https://www.capcut.com/resource/online-subtitle-generator', '_blank');
+}
+
+function openWhisperAI() {
+    window.open('https://freesubtitles.ai', '_blank');
+}
+
+function openWhisperLarge() {
+    window.open('https://anotepad.com/notes/cx7k57eh', '_blank');
+}
+
+function toggleCapcutOptions() {
+    const options = document.getElementById('capcut-options');
+    options.style.display = options.style.display === 'none' || options.style.display === '' ? 'block' : 'none';
+}
